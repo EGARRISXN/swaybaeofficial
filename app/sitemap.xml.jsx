3584ad0a357a37ca0e904d4f03342c8192a9e713
EGARRISXN <egarrisxn@gmail.com>
2023-08-20 11:53:38 -0400

    robots and sitemap

--- app/sitemap.xml.jsx
+++ app/sitemap.xml.jsx
@@ -1,5 +1,5 @@
-export default function sitemap() {
-  return [
+export default function Sitemap() {
+  const urls = [
     {
       url: "https://swaybae.net",
       lastModified: new Date(),
@@ -29,4 +29,28 @@ export default function sitemap() {
       lastModified: new Date(),
     },
   ];
+
+  const currentDate = new Date().toISOString().split("T")[0]; // Format: YYYY-MM-DD
+
+  const sitemapContent = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls
+        .map(
+          (entry) => `
+          <url>
+            <loc>${entry.url}</loc>
+            <lastmod>${entry.lastModified.toISOString().split("T")[0]}</lastmod>
+          </url>
+        `
+        )
+        .join("")}
+    </urlset>
+  `;
+
+  return (
+    <div>
+      <pre>{sitemapContent}</pre>
+    </div>
+  );
 }
