88bceb7be1741367a66082e6a1f05c30cecf984d
EGARRISXN <EGARRISXN@gmail.com>
2023-11-07 19:23:13 -0500

    hype

---
+++ app/hypechallenge/page.jsx
@@ -0,0 +1,10 @@
+import HypeChallenge from "../../components/HypeChallenge";
+
+export default function HypeChallengePage() {
+  return (
+    <main className="mt-5">
+      <h1 className="hidden">Hype Challenge</h1>
+      <HypeChallenge />
+    </main>
+  );
+}
--- components/HypeChallenge.jsx
+++ components/HypeChallenge.jsx
@@ -1,49 +1,39 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import HypeGoals from "../public/images/hypegoals.png";
-// import HypeLogo from "../public/images/hypelogo.png";
-// import HypeCard from "../components/(hypechallenge)/HypeCard";
+import Image from "next/image";
+import HypeGoals from "../public/images/hypegoals.png";
+import HypeLogo from "../public/images/hypelogo.png";
 
-// export default function HypeChallenge() {
-//   return (
-//     <div id="about" className="w-full">
-//       <div className="max-w-[1420px] mx-auto py-16">
-//         <div className="flex flex-col items-center mt-4 xs:px-2 sm:px-6 md:px-12 lg:px-16">
-//           <div className="x">
-//             <Link href="https://twitch.tv/sway_bae">
-//               <Image
-//                 src={HypeLogo}
-//                 quality={100}
-//                 width={830}
-//                 height={448}
-//                 alt="HypeLogo"
-//                 className=" h-[80%] w-[80%] mx-auto"
-//                 priority
-//               />
-//             </Link>
-//           </div>
+export default function HypeChallenge() {
+  return (
+    <div id="about" className="w-full">
+      <div className="max-w-[1420px] mx-auto py-16">
+        <div className="flex flex-col items-center mt-4 xs:px-2 sm:px-6 md:px-12 lg:px-16">
+          <div className="x">
+            <Image
+              src={HypeLogo}
+              quality={100}
+              width={830}
+              height={448}
+              alt="HypeLogo"
+              className="x"
+              priority
+            />
+          </div>
 
-//           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//             <div className="x">
-//               <div className="border-[8px] lg:border-[10px] border-info rounded-xl shadow-xl shadow-purple-800/50">
-//                 <Image
-//                   src={HypeGoals}
-//                   quality={100}
-//                   width={1080}
-//                   height={1080}
-//                   alt="HypeGoals"
-//                   className="x"
-//                   priority
-//                 />
-//               </div>
-//             </div>
-
-//             <div className="x">
-//               <HypeCard />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+          <div className="x">
+            <div className="border-[8px] lg:border-[10px] border-info rounded-xl shadow-xl shadow-purple-800/50">
+              <Image
+                src={HypeGoals}
+                quality={100}
+                width={1080}
+                height={1080}
+                alt="HypeGoals"
+                className="x"
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
