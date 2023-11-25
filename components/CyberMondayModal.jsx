df4e92ffc3cb2423743f9a33ae1951c0af5bfdb9
EGARRISXN <EGARRISXN@gmail.com>
2023-11-25 18:51:32 -0500

    edited modal

--- components/CyberMondayModal.jsx
+++ components/CyberMondayModal.jsx
@@ -12,8 +12,8 @@ export default function CyberMondayModal() {
       className="relative z-[100]"
     >
       <div className="fixed inset-0 flex w-screen justify-center p-8">
-        <Dialog.Overlay className="fixed inset-0 bg-black bg-blur opacity-80 z-[100]" />
-        <Dialog.Panel className="w-fit border-4 rounded-md border-blue-500 bg-transparent z-[500]">
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-80 z-[100]" />
+        <Dialog.Panel className="w-fit max-w-sm z-[500] border-4">
           <video
             autoPlay
             muted
@@ -22,11 +22,11 @@ export default function CyberMondayModal() {
             src="videos/CyberMondayVideo.mov"
             type="video/mp4"
             title="Cyber Monday Sale"
-            className="w-full h-full z-[500] rounded-sm bg-base-100 p-4"
+            className="w-full h-full z-[500]"
           ></video>
           <button
             onClick={() => setIsOpen(false)}
-            className="flex justify-center items-center absolute top-3 right-3 w-10 h-10 border-4 border-blue-400 bg-base-100 rounded-full text-base-content shadow-3xl hover:bg-pink-200 transition duration-300 ease-in-out z-[1000]"
+            className="flex justify-center items-center absolute top-3 right-3 w-10 h-10 bg-base-100 rounded-full text-base-content shadow-xl hover:bg-pink-200 transition duration-300 ease-in-out z-[1000]"
           >
             X
           </button>
