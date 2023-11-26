95c28b5a8ce36d7c29aff1581634a9e75ce151f0
EGARRISXN <EGARRISXN@gmail.com>
2023-11-26 00:27:24 -0500

    okay okay. NOW im done.

--- components/(socials)/GoogleCalendarEmbed.jsx
+++ components/(socials)/GoogleCalendarEmbed.jsx
@@ -1,12 +1,12 @@
 export default function GoogleCalendarEmbed() {
   return (
-    <div className="border-[12px] border-info shadow-xl shadow-purple-800/50">
-      <div className="aspect-w-22 aspect-h-30 sm:aspect-w-30 sm:aspect-h-22">
+    <div className="max-w-6xl md:mb-12 rounded-lg grid p-1 mx-auto shadow-xl">
+      <div className="aspect-w-9 aspect-h-16 md:aspect-w-16 md:aspect-h-9 rounded-lg border-2 shadow-xl border-white">
         <iframe
           src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showPrint=0&showTitle=0&showNav=1&showDate=1&showCalendars=0&src=NjM3Mzk0MzZhMmVhMTIwYTM0MWVjMTNhYmZmYzRiMTI4OWY3NzI0MGE4MzdkMTIwNDNiZWQ4MWNlNmM3OGUxY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5"
           width="100%"
           height="100%"
-          className="rounded-b-sm shadow-xl shadow-purple-800/50"
+          className="h-full w-full border-2 border-white shadow-xl rounded-lg p-1"
         ></iframe>
       </div>
     </div>
--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -3,11 +3,9 @@ import GoogleCalendarEmbed from "../components/(socials)/GoogleCalendarEmbed";
 export default function Calendar() {
   return (
     <div id="about" className="w-full">
-      <div className="max-w-[1420px] mx-auto py-16">
-        <div className="flex flex-col-reverse mx-auto justify-center mt-16  px-2 sm:px-6 md:px-12 lg:px-16 sm:gap-y-12 md:flex-row md:space-x-8">
-          <div className=" shadow-purple-800/50">
-            <GoogleCalendarEmbed />
-          </div>
+      <div className="max-w-[1420px]">
+        <div className="p-1 shadow-xl rounded-lg">
+          <GoogleCalendarEmbed />
         </div>
       </div>
     </div>
