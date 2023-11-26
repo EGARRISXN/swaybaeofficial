eb0d7c725ac6a0939946bcff5324c727dde1096c
EGARRISXN <EGARRISXN@gmail.com>
2023-11-26 00:35:51 -0500

    done done done.

--- components/(socials)/GoogleCalendarEmbed.jsx
+++ components/(socials)/GoogleCalendarEmbed.jsx
@@ -1,6 +1,6 @@
 export default function GoogleCalendarEmbed() {
   return (
-    <div className="max-w-6xl md:mb-12 rounded-lg grid p-1 mx-auto shadow-xl">
+    <div className="md:mb-12 rounded-lg grid grid-cols-1 p-1 mx-auto shadow-xl">
       <div className="aspect-w-9 aspect-h-16 md:aspect-w-16 md:aspect-h-9 rounded-lg border-2 shadow-xl border-white">
         <iframe
           src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showPrint=0&showTitle=0&showNav=1&showDate=1&showCalendars=0&src=NjM3Mzk0MzZhMmVhMTIwYTM0MWVjMTNhYmZmYzRiMTI4OWY3NzI0MGE4MzdkMTIwNDNiZWQ4MWNlNmM3OGUxY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5"
--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -3,8 +3,8 @@ import GoogleCalendarEmbed from "../components/(socials)/GoogleCalendarEmbed";
 export default function Calendar() {
   return (
     <div id="about" className="w-full">
-      <div className="max-w-[1420px]">
-        <div className="p-1 shadow-xl rounded-lg">
+      <div className="max-w-[1420px] mx-auto">
+        <div className="px-1 md:px-16">
           <GoogleCalendarEmbed />
         </div>
       </div>
